2e6367e9eebc347bbde8b9586c5a51b308bbd76c
Andra <94204279+asteele1493@users.noreply.github.com>
2023-07-25 19:19:21 -0700

    Reconfigured JSON file; switched from personal to org

--- components/RegularMargin.js
+++ components/RegularMargin.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
-import { TextInput } from "react-native-paper";
+import React, { useState } from 'react';
+import { View, StyleSheet, Text } from 'react-native'; // Import StyleSheet from react-native
+import { TextInput } from 'react-native-paper';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: "#CCF1FF",
+    backgroundColor: '#CCF1FF',
   },
   row: {
     marginBottom: 16,
@@ -14,30 +14,27 @@ const styles = StyleSheet.create({
 });
 
 export default RegularMargin = () => {
-  const [quantity, setQuantity] = useState("");
-  const [casePrice, setCasePrice] = useState("");
-  const [costPerUnit, setCostPerUnit] = useState("");
-  const [price, setPrice] = useState("");
+  const [quantity, setQuantity] = useState('');
+  const [casePrice, setCasePrice] = useState('');
+  // const [costPerUnit, setCostPerUnit] = useState(0);
 
   // Helper function to check if a value is numeric
-  // const isNumeric = (value) => {
-  //   return /^\d+$/.test(value);
-  // };
+  const isNumeric = (value) => {
+    return /^\d+$/.test(value);
+  };
 
+  // Calculate the cost per unit and price
   const calculateValues = () => {
-    if (quantity && casePrice) {
-      const costPerUnit = (
-        parseFloat(casePrice) / parseFloat(quantity)
-      ).toFixed(2);
-      const price = (costPerUnit / 0.6).toFixed(2);
-
-      setCostPerUnit(costPerUnit);
-      setPrice(price);
+    if (isNumeric(quantity) && isNumeric(casePrice)) {
+      const costPerUnit = casePrice / quantity;
+      const price = costPerUnit / 0.60;
+      return { costPerUnit, price };
     } else {
-      setCostPerUnit("0.00");
-      setPrice("0.00");
+      return { costPerUnit: 0, price: 0 };
     }
   };
+
+  const { costPerUnit, price } = calculateValues();
 
   return (
     <View style={styles.container}>
@@ -45,13 +42,12 @@ export default RegularMargin = () => {
         <Text>Regular Price, 40% Margin</Text>
         <TextInput
           label="Quantity"
-          value={quantity}
-          onChangeText={(input) => {
-            // Only update the quantity state if the input is numeric or empty
-            if (/^[+]?\d*(\.\d*)?$/.test(input)) {
-              setQuantity(input);
+          value={quantity.toString()}
+          onChangeText={(text) => {
+            if (isNumeric(text)) {
+              setQuantity(parseFloat(text));
             } else {
-              setQuantity(""); // Set to empty string if the input is invalid
+              setQuantity(0);
             }
           }}
           keyboardType="numeric"
@@ -60,23 +56,26 @@ export default RegularMargin = () => {
       <View style={styles.row}>
         <TextInput
           label="Case Price"
-          value={casePrice}
-          onChangeText={(input) => {
-            // Only update the casePrice state if the input is numeric or empty
-            if (/^[+]?\d*(\.\d*)?$/.test(input)) {
-              setCasePrice(input);
+          value={casePrice.toString()}
+          onChangeText={(text) => {
+            if (isNumeric(text)) {
+              setCasePrice(parseFloat(text));
             } else {
-              setCasePrice(""); // Set to empty string if the input is invalid
+              setCasePrice(0);
             }
           }}
           keyboardType="numeric"
         />
       </View>
       <View style={styles.row}>
-        <TextInput label="Cost Per Unit" value={costPerUnit} disabled />
+        <TextInput
+          label="Cost Per Unit"
+          value={costPerUnit.toFixed(2)}
+          disabled
+        />
       </View>
       <View style={styles.row}>
-        <TextInput label="Price" value={price} disabled />
+        <TextInput label="Price" value={price.toFixed(2)} disabled />
       </View>
     </View>
   );
